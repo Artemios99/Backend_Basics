059407b99a2cfa15a85817a811714a668f2f1ed0
Artemios99 <artemios.kousiappas3@gmail.com>
2026-07-28 17:31:58 +0300

    Enable CORS for frontend connection

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const express = require('express')
 const mysql = require('mysql2')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
